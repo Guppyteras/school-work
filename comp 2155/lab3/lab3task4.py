d0825dc3b0fa2b79cd53f1471a63294230244652
Guppyteras <caporuscio.amedeo@gmail.com>
2024-11-30 10:32:27 -0500

    Delete comp 2155 directory

--- comp 2155/lab3/lab3task4.py
+++
@@ -1,9 +0,0 @@
-from lab3task3 import Home  # Import the Home class
-
-# Create a new Home object
-home = Home("123 Main St", 200000, 1500)
-
-# Output the three private properties
-print("Address:", home._address)
-print("Price:", home._price)
-print("Size:", home._size)
